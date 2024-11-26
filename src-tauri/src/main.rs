1015a64fba552ff759fe4372345467ad248b0159
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-11-26 22:42:54 +0900

    upgrade: tauri from v1 to v2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,12 +2,21 @@
 
 #[tauri::command]
 fn send_notice(message: &str) {
-  println!("{message}");
+    println!("{message}");
 }
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![send_notice])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_http::init())
+        .invoke_handler(tauri::generate_handler![send_notice])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/components/SendNotice.tsx
+++ src/components/SendNotice.tsx
@@ -1,19 +1,16 @@
 'use client';
 
-import { invoke } from "@tauri-apps/api";
-import { isPermissionGranted, requestPermission, sendNotification } from "@tauri-apps/api/notification";
+import { isPermissionGranted, requestPermission, sendNotification } from "@tauri-apps/plugin-notification";
 
 export default function SendNoticeTestButton({ message = "Test" }: { message?: string }) {
   const handleClick = async () => {
-    await invoke<void>("send_notice", { message }).catch(console.error);
-
     let permissionGranted = await isPermissionGranted();
     if (!permissionGranted) {
       const permission = await requestPermission();
       permissionGranted = permission === 'granted';
     }
     if (permissionGranted) {
-      sendNotification({ title: 'TAURI', body: 'Tauri is awesome!' });
+      sendNotification({ title: 'TAURI', body: message });
     }
   }
 
