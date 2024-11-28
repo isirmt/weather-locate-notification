0ed19f899eae24878b53b63f1925c27ffa8f2cb4
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-11-28 17:12:43 +0900

    add: background process

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -1,4 +1,51 @@
 'use client';
+import { SendNotice } from "@/lib/NoticeManager";
+import { RootState } from "@/lib/PointReduxManager";
+import { GetPrediction } from "@/lib/weather";
+import { GetRainfallData } from "@/lib/WeatherIndexGetter";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useOverlay } from "../overlay/OverlayProvider";
+import { WeatherPoint } from "@/types/WeatherPoint";
+
 export default function Background() {
+  const points = useSelector((state: RootState) => state.pointsState.points);
+  const { openOverlay } = useOverlay();
+
+  useEffect(() => {
+    const intervalId = setInterval(async () => {
+      const rainComingSpots: WeatherPoint[] = [];
+      await Promise.all(
+        points.map(async (point) => {
+          const data = await GetPrediction(point.latitude, point.longitude, "Asia/Tokyo");
+          const futureData = GetRainfallData(data, 30);
+          if (!futureData) return;
+          const currentData = data[futureData.index - 1];
+          if (currentData.value === 0 && futureData.data.value > 0) {
+            // if (true) {
+            rainComingSpots.push(point);
+          }
+        })
+      );
+
+      if (rainComingSpots.length > 0) {
+        SendNotice({
+          title: "◆◇◆◇天気通知◇◆◇◆",
+          "body": `${30}分後に雨が降ります！お気をつけて！☔\n対象地域: ${rainComingSpots.map((spot) => spot.name).join("/")}`
+        })
+        openOverlay(
+          <div>
+            {30}分後に雨が降り出します！☔<br />
+            対象地域: {rainComingSpots.map((spot) => spot.name).join("/")}
+          </div>
+        );
+      }
+
+
+    }, 60 * 1000);
+
+    return () => clearInterval(intervalId);
+  }, [openOverlay, points]);
+
   return null;
 }
---
+++ src/lib/NoticeManager.ts
@@ -0,0 +1,12 @@
+import { isPermissionGranted, Options, requestPermission, sendNotification } from "@tauri-apps/plugin-notification";
+
+export async function SendNotice(options: Options | string) {
+  let permissionGranted = await isPermissionGranted();
+  if (!permissionGranted) {
+    const permission = await requestPermission();
+    permissionGranted = permission === 'granted';
+  }
+  if (permissionGranted) {
+    sendNotification(options);
+  }
+}
