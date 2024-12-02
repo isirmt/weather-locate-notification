5dad75fe21912d112678c24279f84621cf763e16
isirmt - Ito Miri <52734487+isirmt@users.noreply.github.com>
2024-12-02 10:07:17 +0900

    refactor: making array of navLinks

--- src/components/sidebar/Navigation.tsx
+++ src/components/sidebar/Navigation.tsx
@@ -2,6 +2,27 @@
 import React, { useState } from "react";
 import { SidebarItem } from "./SiderbarItem";
 
+const tabLinks = [
+  {
+    href: "/",
+    node: <React.Fragment>
+      <span className="i-tabler-home" />ホーム
+    </React.Fragment>
+  },
+  {
+    href: "/list",
+    node: <React.Fragment>
+      <span className="i-tabler-cloud" />一覧表示
+    </React.Fragment>
+  },
+  {
+    href: "/settings",
+    node: <React.Fragment>
+      <span className="i-tabler-settings" />設定
+    </React.Fragment>
+  },
+]
+
 export default function SidebarNavigation() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
@@ -17,19 +38,11 @@ export default function SidebarNavigation() {
       <div className="h-full px-3 pt-14 pb-4 overflow-y-auto bg-gray-50">
         <ul>
           <li>
-            <SidebarItem href="/">
-              <span className="i-tabler-home" />ホーム
-            </SidebarItem>
-          </li>
-          <li>
-            <SidebarItem href="/list">
-              <span className="i-tabler-cloud" />一覧表示
-            </SidebarItem>
-          </li>
-          <li>
-            <SidebarItem href="/settings">
-              <span className="i-tabler-settings" />設定
-            </SidebarItem>
+            {tabLinks.map((tabLink, i) => (
+              <SidebarItem key={i} href={tabLink.href}>
+                {tabLink.node}
+              </SidebarItem>
+            ))}
           </li>
         </ul>
       </div>
